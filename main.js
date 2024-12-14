7e24c1c6b8be15d52765ea7f4f2d8627a9ab9112
brandtryan <emailme@brandtryan.com>
2024-12-14 07:32:30 -0500

    started

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 // import { htmlString } from "./htmlString.mjs";
 
 const html = `
-<section data-type="narrow"><p>Lorem ipsum dolor sit amet.</p>
+<div class="plate">
+<section><p>Lorem ipsum dolor sit amet.</p>
 </section>
-<section data-type="medium"><p>Lorem ipsum dolor sit amet.</p>
+</div>
+<div class="plate">
+<section><p>Lorem ipsum dolor sit amet.</p>
 </section>
-<section data-type="wide"><p>Lorem ipsum dolor sit amet.</p>
+</div>
+<div class="plate">
+<section><p>Lorem ipsum dolor sit amet.</p>
 </section>
+</div>
 `;
 
 const main = document.createElement('main');
 document.body.appendChild(main);
 
 main.innerHTML = html;
-
-
-const paragraphs = document.getElementsByTagName('p');
-
-console.log(paragraphs);
