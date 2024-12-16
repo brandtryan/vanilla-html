ec0672062a5fe47d7f3044d1c583817fa2d42758
brandtryan <emailme@brandtryan.com>
2024-12-16 08:01:24 -0500

    Add spans for every word

---
+++ spans.js
@@ -0,0 +1,137 @@
+[
+  "<span p00-ln00-w0>My</span>",
+  "<span p00-ln01-w1>my</span>",
+  "<span p00-ln02-w2>they</span>",
+  "<span p00-ln03-w3>but</span>",
+  "<span p00-ln04-w4>test</span>",
+  "<span p00-ln05-w5>mean</span>",
+  "<span p00-ln06-w6>smiles,</span>",
+  "<span p00-ln07-w7>else</span>",
+  "<span p00-ln08-w8>and</span>"
+]
+[
+  "<span p01-ln00-w0>I</span>",
+  "<span p01-ln01-w1>have</span>",
+  "<span p01-ln02-w2>church</span>",
+  "<span p01-ln03-w3>didn't</span>",
+  "<span p01-ln04-w4>everything.</span>",
+  "<span p01-ln05-w5>question</span>",
+  "<span p01-ln06-w6>talkin'</span>",
+  "<span p01-ln07-w7>for</span>",
+  "<span p01-ln09-w8>how</span>",
+  "<span p01-ln10-w9>too</span>",
+  "<span p01-ln11-w10>out</span>",
+  "<span p01-ln12-w11>kinds</span>",
+  "<span p01-ln13-w12>with</span>",
+  "<span p01-ln14-w13>me</span>",
+  "<span p01-ln15-w14>Mass,</span>",
+  "<span p01-ln16-w15>maybe</span>",
+  "<span p02-ln00-w16>I</span>"
+]
+[
+  "<span p02-ln01-w0>prayed</span>",
+  "<span p02-ln02-w1>and</span>",
+  "<span p02-ln03-w2>He</span>",
+  "<span p02-ln04-w3>if</span>",
+  "<span p02-ln05-w4>told</span>",
+  "<span p02-ln06-w5>Masses,</span>",
+  "<span p02-ln07-w6>I</span>",
+  "<span p02-ln08-w7>were</span>",
+  "<span p02-ln09-w8>something</span>",
+  "<span p02-ln10-w9>one</span>",
+  "<span p02-ln11-w10>his</span>",
+  "<span p03-ln00-w11>The</span>"
+]
+[
+  "<span p03-ln01-w0>used</span>",
+  "<span p03-ln02-w1>dad</span>",
+  "<span p03-ln03-w2>an</span>",
+  "<span p03-ln04-w3>in</span>",
+  "<span p03-ln05-w4>asshole?</span>",
+  "<span p03-ln06-w5>like</span>",
+  "<span p03-ln07-w6>have</span>",
+  "<span p04-ln00-w7>My</span>"
+]
+[
+  "<span p04-ln01-w0>wrong</span>",
+  "<span p04-ln02-w1>feet</span>",
+  "<span p04-ln03-w2>Sometimes</span>",
+  "<span p04-ln04-w3>like</span>",
+  "<span p05-ln00-w4>At</span>"
+]
+[
+  "<span p05-ln01-w0>the</span>",
+  "<span p05-ln02-w1>before</span>",
+  "<span p05-ln03-w2>old</span>",
+  "<span p05-ln04-w3>he</span>",
+  "<span p05-ln05-w4>time</span>",
+  "<span p05-ln06-w5>leg</span>",
+  "<span p05-ln07-w6>up</span>",
+  "<span p05-ln08-w7>the</span>",
+  "<span p05-ln09-w8>little</span>",
+  "<span p05-ln10-w9>heard</span>",
+  "<span p05-ln11-w10>and</span>",
+  "<span p05-ln12-w11>different</span>",
+  "<span p05-ln13-w12>out</span>",
+  "<span p05-ln14-w13>would</span>",
+  "<span p05-ln15-w14>sectioning</span>",
+  "<span p06-ln00-w15>Rivi</span>"
+]
+[
+  "<span p06-ln01-w0>my</span>",
+  "<span p06-ln02-w1>all</span>",
+  "<span p06-ln03-w2>types</span>",
+  "<span p06-ln04-w3>where</span>",
+  "<span p06-ln05-w4>gets</span>",
+  "<span p06-ln06-w5>the</span>",
+  "<span p06-ln07-w6>jackbox</span>",
+  "<span p06-ln08-w7>there</span>",
+  "<span p06-ln09-w8>baby</span>",
+  "<span p06-ln10-w9>and</span>",
+  "<span p07-ln00-w10>Anyway,</span>"
+]
+[
+  "<span p07-ln01-w0>weird</span>",
+  "<span p07-ln02-w1>makin'</span>",
+  "<span p07-ln03-w2>looked</span>",
+  "<span p07-ln04-w3>barked</span>",
+  "<span p07-ln05-w4>right</span>",
+  "<span p07-ln06-w5>and</span>",
+  "<span p07-ln07-w6>somethin'.</span>",
+  "<span p08-ln00-w7>The</span>"
+]
+[
+  "<span p08-ln01-w0>lookin'</span>",
+  "<span p08-ln02-w1>he</span>",
+  "<span p08-ln03-w2>or</span>",
+  "<span p08-ln04-w3>because</span>",
+  "<span p08-ln05-w4>been</span>",
+  "<span p08-ln06-w5>gave’em</span>",
+  "<span p08-ln07-w6>him.</span>",
+  "<span p08-ln08-w7>the</span>",
+  "<span p08-ln09-w8>was</span>",
+  "<span p08-ln10-w9>like</span>",
+  "<span p08-ln11-w10>on</span>",
+  "<span p09-ln00-w11>In</span>"
+]
+[
+  "<span p09-ln01-w0>the</span>",
+  "<span p09-ln02-w1>missiles,’cause</span>",
+  "<span p09-ln03-w2>and</span>",
+  "<span p09-ln04-w3>us.</span>",
+  "<span p09-ln05-w4>ever</span>",
+  "<span p09-ln06-w5>the</span>",
+  "<span p09-ln07-w6>Methodist</span>",
+  "<span p09-ln08-w7>I</span>",
+  "<span p09-ln09-w8>steeple,</span>",
+  "<span p09-ln10-w9>behind</span>",
+  "<span p09-ln11-w10>and</span>",
+  "<span p09-ln12-w11>whenever</span>",
+  "<span p09-ln13-w12>I</span>",
+  "<span p09-ln14-w13>and</span>",
+  "<span p09-ln15-w14>launched</span>",
+  "<span p09-ln16-w15>for</span>",
+  "<span p09-ln17-w16>that</span>",
+  "<span p09-ln18-w17>only</span>",
+  "<span p10-ln00-w18>On</span>"
+]
