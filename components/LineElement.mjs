1d989bff903393506bc77302cf290d678a3f2519
brandtryan <emailme@brandtryan.com>
2024-12-19 22:44:20 -0500

    spans

--- components/LineElement.mjs
+++
@@ -1,9 +0,0 @@
-class LineElement extends HTMLElement {
-  connectedCallback() {
-    const shadowroot = this.attachShadow({ mode: open });
-    const pElement = document.createElement('p');
-    shadowroot.appendChild(pElement);
-  }
-}
-
-customElements.define('line-element', LineElement);
