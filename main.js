4055a431db7c0c9bfb8229bae2a3d897836de544
brandtryan <emailme@brandtryan.com>
2024-12-23 18:52:18 -0500

    lucid confusion

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 let finalIds = [];
-// Paragraphs
+// Paragraphs = Divs
 const divsCollection = document.getElementsByTagName('div');
-const divsArray = [];
-const divIdsArray = [];
+const divsArray = [...divsCollection];
+console.log(`number of paragraphs =: ${divsArray.length}`);
 
-// Lines
+// Lines = paragraph
 const linesCollection = document.getElementsByTagName('p');
-const linesArray = [];
+// const linesArray = [];
+const wholeLinesArray = [];
+const splitLinesArray = [];
+const wordsInEachLine = [];
 const wordIdsArray = [];
+const spansArray = [];
 
 for (let line of linesCollection) {
-	linesArray.push(line.textContent.split(' '));
+	wholeLinesArray.push(line);
+	splitLinesArray.push(line.textContent.split(' '));
 	wordIdsArray.push(line.id);
 }
+console.log(`Number of Lines: ${splitLinesArray.length}`);
+console.log(`Number of words in line 0: ${splitLinesArray[0].length}`);
+
+console.log(`word id index 0: ${wordIdsArray[0]}`);
+
+// console.log(`word id length: ${wordIdsArray[0].length}`);
 
 for (let item of wordIdsArray) {
 	for (let i = 0; i < item.length; i++) {
@@ -20,21 +31,27 @@ for (let item of wordIdsArray) {
 		finalIds.push(id);
 	}
 }
-console.log(finalIds);
+console.log(finalIds[0]);
+console.log(`Number of finalIds: ${finalIds.length}`);
+// console.log(wordIdsArray[0]);
+// console.log(wordIdsArray.length);
 
 // Words
 
-const spansArray = [];
-const elementsArray = [];
+// const spansArray = [];
+// const elementsArray = [];
 
-for (let line of linesArray) {
-	for (let i = 0; i < linesArray.length; i++) {
-		spansArray.push(line[i]);
-	}
-}
-console.log(spansArray[0]);
+// for (let line of linesArray) {
+// 	for (let i = 0; i < linesArray.length; i++) {
+// 		spansArray.push(line[i]);
+// 	}
+// }
+// console.log(spansArray[0]);
+// console.log(spansArray.length);
+// console.log(elementsArray.length);
 
-for (let word of spansArray) {
-	elementsArray.push(`<span class='word'>${word}</span>`);
-}
-console.log(elementsArray[0]);
+// for (let word of spansArray) {
+// 	elementsArray.push(`<span class='word'>${word}</span>`);
+// }
+// console.log(elementsArray[0]);
+// console.log(spansArray.length);
