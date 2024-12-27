54907bbac695b42ae8cfdcb6582d67a4024b653f
brandtryan <emailme@brandtryan.com>
2024-12-26 23:32:41 -0500

    prepare for changing all span ids

---
+++ helperFunctions/selectContent.mjs
@@ -0,0 +1,8 @@
+// Functions for selecting part of document to animate
+
+// Animate Section
+
+export function selectContent(id) {
+	const element = document.getElementById(`${id}`);
+	console.log(element.textContent);
+}
