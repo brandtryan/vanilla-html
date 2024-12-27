3ebfb95900da3a833d8aa9f53b8667d95e7243d9
brandtryan <emailme@brandtryan.com>
2024-12-26 19:57:19 -0500

    Gemini

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
-let finalIds = [];
-// Paragraphs = Divs
-const divsCollection = document.getElementsByTagName('div');
-const divsArray = [...divsCollection];
-console.log(`number of paragraphs =: ${divsArray.length}`);
+// let finalIds = [];
+// // Paragraphs = Divs
+// const divsCollection = document.getElementsByTagName('div');
+// const divsArray = [...divsCollection];
+// console.log(`number of paragraphs =: ${divsArray.length}`);
 
-// Lines = paragraph
-const linesCollection = document.getElementsByTagName('p');
-// const linesArray = [];
-const wholeLinesArray = [];
-const splitLinesArray = [];
-const wordsInEachLine = [];
-const wordIdsArray = [];
-const spansArray = [];
+// // Lines = paragraph
+// const linesCollection = document.getElementsByTagName('p');
+// // const linesArray = [];
+// const wholeLinesArray = [];
+// const splitLinesArray = [];
+// const wordsInEachLine = [];
+// const wordIdsArray = [];
+// const spansArray = [];
 
-for (let line of linesCollection) {
-	wholeLinesArray.push(line);
-	splitLinesArray.push(line.textContent.split(' '));
-	wordIdsArray.push(line.id);
-}
-console.log(`Number of Lines: ${splitLinesArray.length}`);
-console.log(`Number of words in line 0: ${splitLinesArray[0].length}`);
+// for (let line of linesCollection) {
+// 	wholeLinesArray.push(line);
+// 	splitLinesArray.push(line.textContent.split(' '));
+// 	wordIdsArray.push(line.id);
+// }
+// console.log(`Number of Lines: ${splitLinesArray.length}`);
+// console.log(`Number of words in line 0: ${splitLinesArray[0].length}`);
 
-console.log(`word id index 0: ${wordIdsArray[0]}`);
+// console.log(`word id index 0: ${wordIdsArray[0]}`);
 
-// console.log(`word id length: ${wordIdsArray[0].length}`);
+// // console.log(`word id length: ${wordIdsArray[0].length}`);
 
-for (let item of wordIdsArray) {
-	for (let i = 0; i < item.length; i++) {
-		let id = item.concat(`-w${i}`);
-		finalIds.push(id);
-	}
-}
-console.log(finalIds[0]);
-console.log(`Number of finalIds: ${finalIds.length}`);
+// for (let item of wordIdsArray) {
+// 	for (let i = 0; i < item.length; i++) {
+// 		let id = item.concat(`-w${i}`);
+// 		finalIds.push(id);
+// 	}
+// }
+// console.log(finalIds[0]);
+// console.log(`Number of finalIds: ${finalIds.length}`);
+
 // console.log(wordIdsArray[0]);
 // console.log(wordIdsArray.length);
 
@@ -55,3 +56,24 @@ console.log(`Number of finalIds: ${finalIds.length}`);
 // }
 // console.log(elementsArray[0]);
 // console.log(spansArray.length);
+
+const paragraphs = document.querySelectorAll('.paragraph-container');
+
+paragraphs.forEach((paragraph, paragraphIndex) => {
+	const paragraphId = paragraph.id;
+	const lines = paragraph.querySelectorAll('.line');
+
+	lines.forEach((line, lineIndex) => {
+		const lineId = line.id;
+		const words = line.textContent.split(/\s+/);
+
+		words.forEach((word, wordIndex) => {
+			const wordId = `${paragraphId.slice(0, 2)}-ln${lineIndex.toString().padStart(2, '0')}-w${(wordIndex + 1).toString().padStart(2, '0')}`;
+			const wordElement = document.createElement('span');
+			wordElement.id = wordId;
+			wordElement.textContent = word + ' ';
+			line.innerHTML = ''; // Clear the existing text content
+			line.appendChild(wordElement);
+		});
+	});
+});
