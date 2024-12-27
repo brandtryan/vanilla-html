a1b1c8549b70a2aa10c9e27c5a4f78a50deb3c29
brandtryan <emailme@brandtryan.com>
2024-12-27 00:39:17 -0500

    wordIDs completed

---
+++ helperFunctions/setWordIds.mjs
@@ -0,0 +1,9 @@
+// const paragraphs = document.querySelectorAll('.line');
+
+// paragraphs.forEach((paragraph) => {
+// 	const spans = paragraph.querySelectorAll('span');
+// 	spans.forEach((span, index) => {
+// 		const paddedIndex = index.toString().padStart(2, '0');
+// 		span.id = paragraph.id + '-w' + paddedIndex;
+// 	});
+// });
--- main.js
+++ main.js
@@ -1,25 +1,3 @@
 import { selectContent } from './helperFunctions/selectContent.mjs';
 
 const select = selectContent;
-
-// Select Plate
-// TO DO
-// select('s00');
-
-const lines = document.querySelectorAll('.line');
-const spans = document.getElementsByTagName('span');
-
-const line00 = lines[0];
-const line00Spans = line00.children;
-console.log(line00Spans);
-
-// console.log(line01);
-
-// for (let span of line00Spans) {
-// 	span.setAttribute('id', 'p00-ln00-w');
-// 	console.log(span);
-// }
-
-// console.log(lines[0].childElementCount);
-
-console.log(lines[1]);
