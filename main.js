175d5a4c39c07547af8638b1b3177371b25c8ae0
brandtryan <emailme@brandtryan.com>
2024-12-31 18:25:38 -0500

    decent results baseline

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+import { breatheFrames, breatheTiming } from './modules/animations/breathe.mjs';
+
+// "wght"   47    200   900
+// "wdth"   60    60   140
+// "ital"   0    0   12
+// "CONT"   0    100   100
+
+// const line = document.querySelector('#p00-ln00');
+const lines = document.querySelectorAll('p');
+// console.log(lines.length);
+
+for (let line of lines) {
+	line.animate(breatheFrames, breatheTiming);
+}
+
 // const progressbar = document.querySelector('#progress');
 
 // progressbar.style.transformOrigin = '0% 50%';
@@ -12,19 +27,3 @@
 // 		}),
 // 	}
 // );
-
-const line = document.querySelector('#p00-ln00');
-
-const rollingKeyframes = new KeyframeEffect(
-	line,
-	[{ fontVariationSettings: `"wght" 100, "wdth" 75, "ital" 0, "CONT" 0` }, { fontVariationSettings: `"wght" 800, "wdth" 125, "ital" 12, "CONT" 100` }],
-	{
-		duration: 1000,
-		direction: 'alternate',
-		easing: 'ease-in-out',
-		iterations: 'Infinity',
-	}
-);
-
-const rollingAnimation = new Animation(rollingKeyframes, document.timeline);
-rollingAnimation.play();
---
+++ modules/animations/breathe.mjs
@@ -0,0 +1,22 @@
+export const breatheFrames = [
+	{
+		fontWeight: 47,
+		fontStretch: '60%',
+		fontStyle: 'normal',
+		fontVariationSettings: `"cont" 100`,
+	},
+	{
+		fontWeight: 900,
+		fontStretch: '140%',
+		fontStyle: 'italic',
+		fontVariationSettings: `"cont" 0`,
+	},
+];
+
+export const breatheTiming = {
+	delay: 100,
+	direction: 'alternate',
+	duration: 1300,
+	easing: 'cubic-bezier(0.45, 0, 0.55, 1)',
+	iterations: '3',
+};
